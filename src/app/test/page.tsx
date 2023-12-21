85ae85fc7064e1f4d2ae510377165bcad4b1f583
Cameron McGinley <49362691+cameronmcginley@users.noreply.github.com>
2023-12-21 04:13:37 -0600

    First commit

---
+++ src/app/test/page.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import { TestCard } from "@/components/test-card";
+import { ModeToggle } from "@/components/mode-toggle";
+import { RadialGradientBG } from "@/components/radial-gradient-bg";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <RadialGradientBG />
+
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <ModeToggle />
+
+        {/* Empty div of 10 px height */}
+        <div className="h-10" />
+
+        {/* Import test-card component */}
+        <TestCard />
+
+        {/* Div with backdrop blur */}
+        {/* <div className="backdrop-filter backdrop-blur-sm bg-white bg-opacity-20 rounded-md p-4">
+          <div className="text-white">Hello world</div>
+        </div> */}
+      </div>
+    </main>
+  );
+}
